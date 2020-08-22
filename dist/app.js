26121444d1f2895fddbada366be40995faa58370
Wayne Chiu <chaoweichiu@gmail.com>
2020-08-22 10:57:12 +0800

    to use newer version of typescript in order to have optional chaining

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 var Car = (function () {
     function Car(name) {
         this.name = name;
@@ -9,15 +10,19 @@ var Truck = (function () {
     function Truck(name) {
         this.name = name;
     }
-    Truck.prototype.load = function () {
-    };
+    Truck.prototype.load = function () { };
     return Truck;
 }());
-var carA = new Car('Toyota');
-var carB = new Car('Ford');
+var carA = new Car("Toyota");
+var carB = new Car("Ford");
 function printCar(car) {
     if (car instanceof Truck) {
         car.load();
     }
 }
 printCar(carA);
+var person = {
+    name: "person",
+    job: { title: "CEO" },
+};
+console.log((_a = person === null || person === void 0 ? void 0 : person.job) === null || _a === void 0 ? void 0 : _a.title);
--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,29 @@
 class Car {
-  constructor(public name: string){}
+  constructor(public name: string) {}
 }
 
 class Truck {
-  constructor(public name: string){}
+  constructor(public name: string) {}
 
-  load() {
+  load() {}
+}
 
+type Vehicle = Car | Truck;
+
+const carA = new Car("Toyota");
+const carB = new Car("Ford");
+
+function printCar(car: Vehicle) {
+  if (car instanceof Truck) {
+    car.load();
   }
 }
 
-type Vehicle = Car | Truck
+printCar(carA);
 
-const carA = new Car('Toyota')
-const carB = new Car('Ford')
+const person = {
+  name: "person",
+  job: { title: "CEO" },
+};
 
-function printCar(car:Vehicle) {
-  if(car instanceof Truck) {
-    car.load()
-  }
-}
-
-printCar(carA)
+console.log(person?.job?.title)
